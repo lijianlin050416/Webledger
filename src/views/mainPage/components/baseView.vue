ec0a61083fd10005066c72a01fa0edc7d7160b2d
LiJianlin <lijianlin050416@gmail.com>
2024-08-06 18:35:57 +0800

    refactor(baseView): 优化对齐属性和按钮功能
    将对齐属性从字符串改为布尔值，确保弹性盒子正确对齐。
    重构收入和支出对话框的确认按钮，使其更具可读性。
    添加调试按钮，便于开发时查看数据类型。
    
    引入货币类型的枚举，以提高代码的可读性和维护性。
    使用响应式对象来处理表单数据，为收入和支出表单提供更灵活的数据绑定。
    修复了一个导致用户创建失败的bug，正确分配UUID。
    
    此次重构旨在优化代码的可读性和可维护性，同时修复了一些潜在的问题。

--- src/views/mainPage/components/baseView.vue
+++ src/views/mainPage/components/baseView.vue
@@ -17,34 +17,67 @@
       <el-button size="large" color="var(--color-danger)" round @click="outVisible = true">支出</el-button>
     </div>
 
-    <el-dialog v-model="inVisible" title="收入登记" width="300" align-center="true">
+    <el-dialog v-model="inVisible" title="收入登记" width="300" :align-center="true">
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="inVisible = false">取消</el-button>
-          <el-button type="primary" @click="inVisible = false" color="var(--color-base)">
+          <el-button type="primary" @click="handleIn()" color="var(--color-base)">
             确认
           </el-button>
         </div>
       </template>
     </el-dialog>
-    <el-dialog v-model="outVisible" title="支出登记" width="300" align-center="true">
+    <el-dialog v-model="outVisible" title="支出登记" width="300" :align-center="true">
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="outVisible = false">取消</el-button>
-          <el-button type="primary" @click="outVisible = false" color="var(--color-base)">
+          <el-button type="primary" @click="handleOut()" color="var(--color-base)">
             确认
           </el-button>
         </div>
       </template>
     </el-dialog>
+    <button @click="console.log(moneyType.eatAndDrink)">666</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const inVisible = ref(false)
 const outVisible = ref(false)
+
+enum moneyType {
+  eatAndDrink, // 餐饮
+  transpotation, // 通勤
+  entertainment, // 娱乐
+  consumables, // 日常消耗品
+  shopping, // 购买衣物
+  health, // 医疗
+  carCost, // 车辆相关
+}
+
+const inForm = reactive({
+  money: 0,
+  type: 0,
+  date: '',
+  description: ''
+})
+
+const outForm = reactive({
+  money: 0,
+  type: 0,
+  date: '',
+  description: ''
+})
+
+const handleIn = () => {
+  inVisible.value = false
+}
+
+const handleOut = () => {
+  outVisible.value = false
+}
 </script>
 
 <style scoped>
